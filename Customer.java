32fd830d48bbf5cc54ad11cc68dee2fc5f5ad00c
Rbowie100 <Ryan.Bowie321@gmail.com>
2025-10-03 06:44:30 -0400

    Update Customer.java
    Added the ability to get the time the customer has been in the store, what time they have arrived in store and at checkout, and when they left.

--- Customer.java
+++ Customer.java
@@ -1,1 +1,39 @@
 
+public class customer {
+    
+    public int Arrivaltimeminute;
+    public int Arrivaltimehour;
+    public int minute=0;
+    public int entercheckout;
+    public int leavecheckout;
+    time clock;
+    customer(int Arrivaltimeminute, int Arrivaltimehour, time clock){
+        this.Arrivaltimeminute=Arrivaltimeminute;
+        this.Arrivaltimehour=Arrivaltimehour;
+        clock=new time();
+    }
+    public int getarrivalminute(){
+        return Arrivaltimeminute;
+    }
+
+    public int getArrivaltimehour() {
+        return Arrivaltimehour;
+    }
+
+    public int getleave(){
+        return leavecheckout;
+    }
+
+    public int getEntercheckout() {
+        return entercheckout;
+    }
+    public void minuteincrease(){
+        minute+=1;
+        
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+}
+
