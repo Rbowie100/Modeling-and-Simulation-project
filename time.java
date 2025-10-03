ec96cf2dffaeccb6f677456415d36b3ec47e577e
Rbowie100 <Ryan.Bowie321@gmail.com>
2025-10-02 23:49:58 -0400

    Update time.java
    Completed the methods to increase the time, and change A.M and P.M for time as well as get the A.M and P.M

--- time.java
+++ time.java
@@ -1,1 +1,43 @@
+public class time {
+    int minute=0;
+    int hour=6;
+    String Time1="A.M";
+    String Time2="P.M";
+    boolean AM=true;
+    boolean PM=false;
+    int timeend=10;
+    int timestart=6;
+    String[] Days;
+
+    public int Arrivaltimehour (){
+        return hour;
+    }
+    public int Arrivaltimeminute (){
+        return minute;
+    }
+    public String Arrival(){
+        if(AM){
+            return Time1;
+        }
+        if(PM){
+        return Time2;
+        }
+        return "AM";
+    }
+    public void printtime(){
+
+    }
+    public void increasetime(){
+      minute+=1;
+      if(minute==60){
+          hour+=1;
+      }
+      if(hour==12){
+          PM=true;
+          AM=false;
+      }
+
+    }
+   public int leavetime;
+}
 
